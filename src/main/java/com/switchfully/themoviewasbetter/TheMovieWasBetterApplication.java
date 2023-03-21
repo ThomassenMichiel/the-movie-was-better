211b6bb6274c07ef8f590ad4b3ae74a9497b9553
Michiel Thomassen <mich.thomassen@gmail.com>
2023-03-21 14:07:38 +0100

    Initial commit

---
+++ src/main/java/com/switchfully/themoviewasbetter/TheMovieWasBetterApplication.java
@@ -0,0 +1,13 @@
+package com.switchfully.themoviewasbetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheMovieWasBetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheMovieWasBetterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/switchfully/themoviewasbetter/TheMovieWasBetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchfully.themoviewasbetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheMovieWasBetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
