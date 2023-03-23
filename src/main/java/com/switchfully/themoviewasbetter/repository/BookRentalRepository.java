acb497c6fd011bf9fe05c1e8922d1725617199e0
Michiel Thomassen <mich.thomassen@gmail.com>
2023-03-23 18:49:44 +0100

    Completed BookRentalRepositoryTest Normalized names

--- src/main/java/com/switchfully/themoviewasbetter/repository/BookRentalRepository.java
+++ src/main/java/com/switchfully/themoviewasbetter/repository/BookRentalRepository.java
@@ -1,24 +1,33 @@
 package com.switchfully.themoviewasbetter.repository;
 
+import com.switchfully.themoviewasbetter.domain.Book;
 import com.switchfully.themoviewasbetter.domain.BookRental;
+import com.switchfully.themoviewasbetter.domain.Member;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 
 @Component
 public class BookRentalRepository {
-    private HashMap<String, BookRental> repository = new HashMap<>();
+    private final HashMap<String, BookRental> repository;
 
-    public BookRental createRental(BookRental bookRental){
+    public BookRentalRepository() {
+        repository = new HashMap<>();
+        BookRental aBook = new BookRental("0", new Member(), new Book("a book", "", "", "", ""), LocalDate.of(1990,10,10));
+        repository.put(aBook.getId(), aBook);
+    }
+
+    public BookRental create(BookRental bookRental){
         repository.put(bookRental.getId() , bookRental);
         return bookRental;
     }
-    public void deleteRental(BookRental bookRental){
+    public void delete(BookRental bookRental){
         repository.remove(bookRental.getId() , bookRental);
     }
 
-    public List<BookRental> getAllBooks() {
+    public List<BookRental> findAll() {
         return repository.values().stream().toList();
     }
 }
--- src/main/java/com/switchfully/themoviewasbetter/service/BookRentalService.java
+++ src/main/java/com/switchfully/themoviewasbetter/service/BookRentalService.java
@@ -22,26 +22,26 @@ public class BookRentalService {
     }
 
     public List<BookRentalDTO> getAllRentals(){
-        return bookRentalRepository.getAllBooks().stream().map(mapper::toDto).collect(Collectors.toList());
+        return bookRentalRepository.findAll().stream().map(mapper::toDto).collect(Collectors.toList());
     }
     public BookRentalDTO saveRental(BookRentalDTO newRental) {
         BookRental rentalToSave = mapper.toBookRental(newRental);
-        return mapper.toDto(bookRentalRepository.createRental(rentalToSave));
+        return mapper.toDto(bookRentalRepository.create(rentalToSave));
     }
 
     public void returnBookRental(BookRentalDTO bookRentalToReturn){
-        bookRentalRepository.deleteRental(mapper.toBookRental(bookRentalToReturn));
+        bookRentalRepository.delete(mapper.toBookRental(bookRentalToReturn));
     }
 
     public List<BookRentalDTO> getAllBookRentalsByMember(Member member) {
-        return mapper.listToDto(bookRentalRepository.getAllBooks()
+        return mapper.listToDto(bookRentalRepository.findAll()
                 .stream()
                 .filter(x -> member.equals(x.getMember()))
                 .collect(Collectors.toList()));
     }
 
     public List<BookRentalDTO> getAllBookRentalsByDueDate() {
-        return mapper.listToDto(bookRentalRepository.getAllBooks()
+        return mapper.listToDto(bookRentalRepository.findAll()
                 .stream()
                 .filter(x -> LocalDate.now().isAfter(x.getReturnDate()))
                 .collect(Collectors.toList()));
